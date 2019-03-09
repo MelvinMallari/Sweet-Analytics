7c1646fe1ba3e3e840a4f3449289595cc78901b7
Melvin Mallari <mtmallari@ucdavis.edu>
2019-03-09 13:59:13 -0800

    Add Express routing

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
 const db = require('./config/keys').mongoURI;
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,5 +13,14 @@ mongoose
   .then(() => console.log("Connected to MongoDB succesfully"))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send("Hello Sweet Analytics"));
+
+// Parse incoming request bo
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+app.get('/', (req, res) => res.send(`Hello Sweet Analytics`));
 app.listen(port, () => console.log(`Server us running on port ${port}`));
+
