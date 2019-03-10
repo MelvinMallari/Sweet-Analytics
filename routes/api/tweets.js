7160a9016e7b3b65382b5ce8e6f75834939babe5
Melvin Mallari <mtmallari@ucdavis.edu>
2019-03-09 19:41:24 -0800

    Add twitter post auth route

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 
 const Tweet = require('../../models/Tweet');
-const validateTweetInput = require('../../validation/tweets');
+const validateTweetInput = require('../../validation/tweet');
 
 router.get('/', (req, res) => {
   Tweet.find()
@@ -24,5 +24,20 @@ router.get('/:id', (req, res) => {
         .then(tweet => res.json(tweet))
         .catch(err => res.status(404).json({ notweetfound: "No tweet foud with that id" }))
 });
+
+router.post('/',
+  passport.authenticate('jwt', { session: false }), 
+  (req, res) => {
+    const { errors, isValid } = validateTweetInput(req.body);
+
+    if (!isValid) return res.status(400).json(errors);
+    
+    const newTweet = new Tweet({
+      text: req.body.text,
+      user: req.user.id
+    });
+    newTweet.save().then(tweet => res.json(tweet));
+  }
+);
 
 module.exports = router;
